0111a174106b3aaa83df47b3e90d347affd65ab3
DESKTOP-E7MHJI6\pc <patrivkpan98@gmail.com>
2023-05-05 20:44:30 +0800

    [main-front]: connect fron to place and weather apis

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -26,14 +26,35 @@
     </div>
 </html>
 <script>
-	$('.select-place-btn').click(function () {
+	$('.select-place-btn').click(getPlace());
+
+	/**
+	* Get place data for latitude and longitude
+	**/
+	function getPlace()
+	{
 		$.ajax({
 			url: '/api/place/' + $(this).attr('data-place'),
 			success: function(oRes, sStatus) {
 				let oLatLong = oRes['context']['geo_bounds']['circle']['center'];
-				console.log(oRes, sStatus);
 				console.log(oLatLong['latitude'], oLatLong['longitude']);
+				getWeather(oLatLong['latitude'], oLatLong['longitude']);
 			}
 		})
-	});
+	}
+
+	/**
+	* Get weather data using latitude and longitude
+	*/
+	function getWeather(sLat, sLong)
+	{
+		$.ajax({
+			url: '/api/weather/' + sLat + '/' + sLong,
+			success: function(oRes, sStatus) {
+				console.log(oRes)
+			}
+		})
+
+	}
+
 </script>
