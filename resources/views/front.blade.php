13ceebf97b8b62cc4e48b9fb4c8930fa2380f5d5
DESKTOP-E7MHJI6\pc <patrivkpan98@gmail.com>
2023-05-05 20:17:44 +0800

    [main-front]: initial commit

---
+++ resources/views/front.blade.php
@@ -0,0 +1,39 @@
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+</head>
+<html>
+    <div>
+    	<h1>JAPAN SAIKO!</h1>
+        This is the weathering app!
+    </div>
+    <br>
+    <div>
+		Which place do you want to check the weather?
+		@php
+			$aPlaces = [
+				'Tokyo',
+				'Yokohama',
+				'Kyoto',
+				'Osaka',
+				'Sapporo',
+				'Nagoya'
+			];
+		@endphp
+		<br>
+		@foreach ($aPlaces as $sPlace)
+    		<button class="select-place-btn" data-place='{{ $sPlace }}'>{{ $sPlace }}</button>
+		@endforeach
+    </div>
+</html>
+<script>
+	$('.select-place-btn').click(function () {
+		$.ajax({
+			url: '/api/places',
+			success: function(oRes, sStatus) {
+				let oLatLong = oRes['context']['geo_bounds']['circle']['center'];
+				console.log(oRes, sStatus);
+				console.log(oLatLong['latitude'], oLatLong['longitude']);
+			}
+		})
+	});
+</script>
--- resources/views/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-<html>
-    <div>
-    	<h1>JAPAN SAIKO!</h1>
-        This is the weathering app!
-    </div>
-</html>
