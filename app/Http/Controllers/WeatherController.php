b05c901fb2d005e8fafb7d8648bfa09ec87d07ea
DESKTOP-E7MHJI6\pc <patrivkpan98@gmail.com>
2023-05-06 09:19:08 +0800

    [main-improvement]: change temp unit to celsius

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -14,7 +14,7 @@ class WeatherController extends Controller
     public function getWeather(float $sLat, float $sLong)
     {
     	$sAPIKey = env('OPEN_WEATHER_API_KEY', 'dcca5aa68b3b6d3347b4cbbaa2e87fc6');
-    	$sUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' . $sLat . '&lon=' . $sLong . '&appid=' . $sAPIKey;
+    	$sUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' . $sLat . '&lon=' . $sLong . '&appid=' . $sAPIKey . '&units=metric';
 
 		return GuzzleHelper::request('GET', $sUrl);
     }
